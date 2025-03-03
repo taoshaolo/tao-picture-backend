396734cf17f0bb5c9e9526cfc513e36e7a10a0f7
taoshao <taoshaolo@163.com>
2025-03-03 11:09:43 +0800

    优化：以图搜图添加acsToken

--- src/main/java/com/taoshao/taopicture/api/imagesearch/sub/GetImagePageUrlApi.java
+++ src/main/java/com/taoshao/taopicture/api/imagesearch/sub/GetImagePageUrlApi.java
@@ -40,11 +40,14 @@ public class GetImagePageUrlApi {
         long uptime = System.currentTimeMillis();
         // 请求地址
         String url = "https://graph.baidu.com/upload?uptime=" + uptime;
+        String acsToken = "jmM4zyI8OUixvSuWh0sCy4xWbsttVMZb9qcRTmn6SuNWg0vCO7N0s6Lffec+IY5yuqHujHmCctF9BVCGYGH0H5SH/H3VPFUl4O4CP1jp8GoAzuslb8kkQQ4a21Tebge8yhviopaiK66K6hNKGPlWt78xyyJxTteFdXYLvoO6raqhz2yNv50vk4/41peIwba4lc0hzoxdHxo3OBerHP2rfHwLWdpjcI9xeu2nJlGPgKB42rYYVW50+AJ3tQEBEROlg/UNLNxY+6200B/s6Ryz+n7xUptHFHi4d8Vp8q7mJ26yms+44i8tyiFluaZAr66/+wW/KMzOhqhXCNgckoGPX1SSYwueWZtllIchRdsvCZQ8tFJymKDjCf3yI/Lw1oig9OKZCAEtiLTeKE9/CY+Crp8DHa8Tpvlk2/i825E3LuTF8EQfzjcGpVnR00Lb4/8A";
+
 
         try {
             // 2.发送请求
             HttpResponse httpResponse = HttpRequest.post(url)
                     .form(formData)
+                    .header("Acs-Token", acsToken)
                     .timeout(5000)
                     .execute();
             if (httpResponse.getStatus() != HttpStatus.HTTP_OK) {
